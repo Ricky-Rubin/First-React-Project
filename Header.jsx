f8abc06b822c495570adc5ad584b1f8facaee5e4
Owolabi <owolabi1738@gmail.com>
2025-07-22 16:58:40 +0100

    Updated react-logo source path to make it appear in deployment

--- Header.jsx
+++ Header.jsx
@@ -1,7 +1,9 @@
+import logo from './assets/react-logo.png';
+
 function Header() {
     return (
       <header className='header'>
-        <img src="react-logo.png" width="40px" alt="react logo"/>
+        <img src={logo} width="40px" alt="react logo"/>
         <nav>
           <ul className='list'>
             <li>Pricing</li>
